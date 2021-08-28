1d4e3a696b58d681a2df682f13576332a30db57e
HelpJamess <89209981+HelpJamess@users.noreply.github.com>
2021-08-28 19:58:51 -0400

    Created cipher

---
+++ cipher.py
@@ -0,0 +1,29 @@
+asciiLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+asciiUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+newName = []
+
+def listToStr(lst):
+  string = ""
+  for i in lst:
+    string += i
+  return string
+
+def cipher(string, shift):
+  newString = []
+  for i in string:
+    if i == i.lower():
+      index = asciiLower.index(i) + shift
+      letter = asciiLower[(index % len(asciiLower)) - 1]
+      newString.append(letter)
+    if i == i.upper():
+      index = asciiUpper.index(i) + shift
+      letter = asciiUpper[(index % len(asciiUpper)) - 1]
+      newString.append(letter)
+  encryptedString = listToStr(newString)
+  return encryptedString
+
+userInName = input("Enter the phrase you want to ")
+userInShift = int(input("Enter the number of shifts you want the name to go through"))
+
+cryptName = cipher(userInName, userInShift)
+print(cryptName)
